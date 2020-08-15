0e16a65beb4bb465daf98fff6f6f03b405a4557b
Zhbert <zhbert@yandex.ru>
2020-08-15 17:47:20 +0300

    Connect to database, read and write messages into database

--- src/main/java/ru/zhbert/corporatefoodchecker/GreetingController.java
+++ src/main/java/ru/zhbert/corporatefoodchecker/GreetingController.java
@@ -1,26 +1,63 @@
 package ru.zhbert.corporatefoodchecker;
 
+import ru.zhbert.corporatefoodchecker.domain.Message;
+import ru.zhbert.corporatefoodchecker.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Map<String, Object> model) {
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
         model.put("name", name);
         return "greeting";
     }
 
     @GetMapping
-    public  String main(Map<String, Object> model) {
-        model.put("some", "Corporate Food Checker!");
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
         return "main";
     }
 
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
 }
---
+++ src/main/java/ru/zhbert/corporatefoodchecker/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package ru.zhbert.corporatefoodchecker.repos;
+
+import ru.zhbert.corporatefoodchecker.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}

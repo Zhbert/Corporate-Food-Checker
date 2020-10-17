0445a7ac091ad52da1015568de45b656d76f8e86
Zhbert <zhbert@yandex.ru>
2020-10-17 10:34:53 +0300

    Fix logging in start scripts

--- Documentation/Running_Scripts/restart.sh
+++ Documentation/Running_Scripts/restart.sh
@@ -2,5 +2,5 @@
 
 rm /home/zhbert/cfc/logs/log
 pgrep java | xargs kill -9
-nohup java -jar /home/zhbert/cfc/bin/ru.zhbert.corporatefoodchecker-0.0.1.jar > log.txt &
+nohup java -jar /home/zhbert/cfc/bin/ru.zhbert.corporatefoodchecker-0.0.1.jar > /home/zhbert/cfc/logs/log &
 
